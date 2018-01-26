cb46e4b7d3553edabc3c2240a781b7c2aaaa271c
Anduin Mooney <anduinmooney@gmail.com>
2018-01-26 12:38:07 -0800

    Pass getters and setters test for armor.

--- src/main/java/models/Armor.java
+++ src/main/java/models/Armor.java
@@ -1,4 +1,107 @@
 package models;
 
 public class Armor {
+    private String armorName;
+    private String defense;
+    private String soak;
+    private String armorPrice;
+    private String armorEncumberance;
+    private String armorHardPoint;
+    private String armorRarity;
+
+    public Armor(String armorName, String defense, String soak, String armorPrice, String armorEncumberance, String armorHardPoint, String armorRarity) {
+        this.armorName = armorName;
+        this.defense = defense;
+        this.soak = soak;
+        this.armorPrice = armorPrice;
+        this.armorEncumberance = armorEncumberance;
+        this.armorHardPoint = armorHardPoint;
+        this.armorRarity = armorRarity;
+    }
+
+    public String getArmorName() {
+        return armorName;
+    }
+
+    public void setArmorName(String name) {
+        this.armorName = name;
+    }
+
+    public String getDefense() {
+        return defense;
+    }
+
+    public void setDefense(String defense) {
+        this.defense = defense;
+    }
+
+    public String getSoak() {
+        return soak;
+    }
+
+    public void setSoak(String soak) {
+        this.soak = soak;
+    }
+
+    public String getArmorPrice() {
+        return armorPrice;
+    }
+
+    public void setArmorPrice(String price) {
+        this.armorPrice = price;
+    }
+
+    public String getArmorEncumberance() {
+        return armorEncumberance;
+    }
+
+    public void setArmorEncumberance(String armorEncumberance) {
+        this.armorEncumberance = armorEncumberance;
+    }
+
+    public String getArmorHardPoint() {
+        return armorHardPoint;
+    }
+
+    public void setArmorHardPoint(String armorHardPoint) {
+        this.armorHardPoint = armorHardPoint;
+    }
+
+    public String getArmorRarity() {
+        return armorRarity;
+    }
+
+    public void setArmorRarity(String armorRarity) {
+        this.armorRarity = armorRarity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Armor armor = (Armor) o;
+
+        if (armorName != null ? !armorName.equals(armor.armorName) : armor.armorName != null) return false;
+        if (defense != null ? !defense.equals(armor.defense) : armor.defense != null) return false;
+        if (soak != null ? !soak.equals(armor.soak) : armor.soak != null) return false;
+        if (armorPrice != null ? !armorPrice.equals(armor.armorPrice) : armor.armorPrice != null) return false;
+        if (armorEncumberance != null ? !armorEncumberance.equals(armor.armorEncumberance) : armor.armorEncumberance != null)
+            return false;
+        if (armorHardPoint != null ? !armorHardPoint.equals(armor.armorHardPoint) : armor.armorHardPoint != null)
+            return false;
+        return armorRarity != null ? armorRarity.equals(armor.armorRarity) : armor.armorRarity == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = armorName != null ? armorName.hashCode() : 0;
+        result = 31 * result + (defense != null ? defense.hashCode() : 0);
+        result = 31 * result + (soak != null ? soak.hashCode() : 0);
+        result = 31 * result + (armorPrice != null ? armorPrice.hashCode() : 0);
+        result = 31 * result + (armorEncumberance != null ? armorEncumberance.hashCode() : 0);
+        result = 31 * result + (armorHardPoint != null ? armorHardPoint.hashCode() : 0);
+        result = 31 * result + (armorRarity != null ? armorRarity.hashCode() : 0);
+        return result;
+    }
 }
--- src/test/java/models/ArmorTest.java
+++ src/test/java/models/ArmorTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,104 @@ public class ArmorTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getNameReturnsCorrectName() throws Exception {
+        Armor testArmor = setupArmor();
+        assertEquals("Armored Clothing", testArmor.getArmorName());
+    }
+
+    @Test
+    public void getDefenseCorrectDefense() throws Exception {
+        Armor testArmor = setupArmor();
+        assertEquals("1", testArmor.getDefense());
+    }
+
+    @Test
+    public void getSoakReturnsCorrectSoak() throws Exception {
+        Armor testArmor = setupArmor();
+        assertEquals("1", testArmor.getSoak());
+    }
+    @Test
+    public void getPriceReturnsCorrectPrice() throws Exception {
+        Armor testArmor = setupArmor();
+        assertEquals("1000", testArmor.getArmorPrice());
+    }
+
+    @Test
+    public void getEncumberanceReturnsCorrectEncumberance() throws Exception {
+        Armor testArmor = setupArmor();
+        assertEquals("3", testArmor.getArmorEncumberance());
+    }
+
+    @Test
+    public void getHardPointReturnsCorrectHardPoint() throws Exception {
+        Armor testArmor = setupArmor();
+        assertEquals("1", testArmor.getArmorHardPoint());
+    }
+
+    @Test
+    public void getRarityReturnsRarity() throws Exception {
+        Armor testArmor = setupArmor();
+        assertEquals("6", testArmor.getArmorRarity());
+    }
+
+    @Test
+    public void setNameSetsCorrectName() throws Exception {
+        Armor testArmor = setupArmor();
+        testArmor.setArmorName("Heavy Clothing");
+        assertNotEquals("Blaster Rifle",testArmor.getArmorName());
+    }
+
+    @Test
+    public void setDefenseSetsCorrectDefense() throws Exception {
+        Armor testArmor = setupArmor();
+        testArmor.setDefense("0");
+        assertNotEquals("1", testArmor.getDefense());
+    }
+
+    @Test
+    public void setSoakSetsCorrectSoak() throws Exception {
+        Armor testArmor = setupArmor();
+        testArmor.setSoak("1");
+        assertEquals("1", testArmor.getSoak());
+    }
+    @Test
+    public void setPriceSetsCorrectPrice() throws Exception {
+        Armor testArmor = setupArmor();
+        testArmor.setArmorPrice("50");
+        assertNotEquals("1000", testArmor.getArmorPrice());
+    }
+
+    @Test
+    public void setEncumberanceSetsCorrectEncumberance() throws Exception {
+        Armor testArmor = setupArmor();
+        testArmor.setArmorEncumberance("1");
+        assertNotEquals("3", testArmor.getArmorEncumberance());
+    }
+
+    @Test
+    public void setHardPointSetsCorrectHardPoint() throws Exception {
+        Armor testArmor = setupArmor();
+        testArmor.setArmorHardPoint("0");
+        assertNotEquals("1",testArmor.getArmorHardPoint());
+    }
+
+    @Test
+    public void setRaritySetsCorrectRarity() throws Exception {
+        Armor testArmor = setupArmor();
+        testArmor.setArmorRarity("0");
+        assertNotEquals("6",testArmor.getArmorRarity());
+    }
+    
+    public Armor setupArmor (){
+        return new Armor("Armored Clothing", "1", "1", "1000", "3", "1", "6");
+
+    }
+
+    public Armor setupAltArmor (){
+        return new Armor("Heavy Clothing", "0", "1", "50", "1", "0", "0");
+
     }
 }
