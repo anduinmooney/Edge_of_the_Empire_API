580d085f2b3737fda26c68f6a03504fe3161d447
Anduin Mooney <anduinmooney@gmail.com>
2018-01-26 13:36:44 -0800

    Add Weapon Overrides.

--- src/main/java/dao/Sql2oWeaponDao.java
+++ src/main/java/dao/Sql2oWeaponDao.java
@@ -1,5 +1,82 @@
 package dao;
 
-public class Sql2oWeaponDao {
+import models.Weapon;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+public class Sql2oWeaponDao implements WeaponDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oWeaponDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Weapon weapon) {
+        String sql = "INSERT INTO weapons (name, skill, damage, critical, range, encumberance, hardPoint, rarity, price) VALUES (:name, :skill, :damage, :critical, :range, :encumberance, :hardPoint, :rarity, :price)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql)
+                    .bind(weapon)
+                    .executeUpdate()
+                    .getKey();
+            weapon.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from weapons WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Weapon> getAll() {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM weapons")
+                    .executeAndFetch(Weapon.class);
+        }
+    }
+
+    @Override
+    public void update(int id, String newName, String newSkill, String newDamage, String newCritical, String newRange, String newEncumberance, String newHardPoint, String newRarity, String newPrice){
+        String sql = "UPDATE weapons SET (name, skill, damage, critical, range, encumberance, hardPoint, rarity, price) = (:name, :skill, :damage, :critical, :range, :encumberance, :hardPoint, :rarity, :price) WHERE id=:id";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("name", newName)
+                    .addParameter("skill", newSkill)
+                    .addParameter("damage", newDamage)
+                    .addParameter("critical", newCritical)
+                    .addParameter("range", newRange)
+                    .addParameter("encumberance", newEncumberance)
+                    .addParameter("hardPoint", newHardPoint)
+                    .addParameter("rarity", newRarity)
+                    .addParameter("price", newPrice)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+            System.out.println("error message");
+        }
+    }
+
+    @Override
+    public Weapon findById(int id) {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM weapons WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Weapon.class);
+        }
+    }
 
 }
--- src/main/java/dao/WeaponDao.java
+++ src/main/java/dao/WeaponDao.java
@@ -1,23 +1,22 @@
 package dao;
 
 import models.Weapon;
+import dao.Sql2oWeaponDao;
 
 import java.util.List;
 
 public interface WeaponDao {
     //create
-    void add (Weapon weapon); //J
-    // void addRestaurantToFoodType(Restaurant restaurant, Foodtype foodtype) //D & E
+    void add (Weapon weapon);
 
     //read
-    List<Weapon> getAll(); //A
-    // List<Foodtype> getAllFoodtypesForARestaurant(int restaurantId); //D & E - we will implement this soon.
+    List<Weapon> getAll();
 
-    Weapon findById(int id); //B & C
+    Weapon findById(int id);
 
     //update
-    void update(int id, String name, String skill, String damage, String critical, String range, String encumberance, String hardPoint, String rarity, String price); //L
+    void update(int id, String name, String skill, String damage, String critical, String range, String encumberance, String hardPoint, String rarity, String price);
 
     //delete
-    void deleteById(int id); //K
+    void deleteById(int id);
 }
